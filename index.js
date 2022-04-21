38f762100e5ec02ac2685cbd4f96220ae41878dd
aventusm <8antonm@gmail.com>
2022-04-21 15:55:24 +0300

    Fix issue from previous ZAP scan

--- index.js
+++ index.js
@@ -4,11 +4,28 @@ const helmet = require('helmet');
 const app = express();
 app.use(helmet());
 app.disable('X-powered-by');
-/* app.use(function (req, res, next) {
-  res.header('X-powered-by', 'Blood, sweat, and tears');
-  res.header('X-Content-Type-Options', 'nosniff');
+
+// https://regbrain.com/article/cache-headers-express-js
+// Fix the following issue: ZAP Scan Baseline Report #11
+const setCache = function (req, res, next) {
+  // here you can define period in second, this one is 5 minutes
+  const period = 60 * 5;
+
+  // you only want to cache for GET requests
+  if (req.method == 'GET') {
+    res.set('Cache-control', `public, max-age=${period}`);
+  } else {
+    // for the other requests set strict no caching parameters
+    res.set('Cache-control', 'no-store');
+  }
+
+  // remember to call next() to pass on the request
   next();
-}); */
+};
+
+// now call the new middleware function in your app
+
+app.use(setCache);
 
 app.get('/', (_req, res) => {
   console.log('Accessed!');
