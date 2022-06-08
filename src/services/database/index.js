32621270591063b50e787b8d2afdcc24c89148d9
Anton <8antonm@gmail.com>
2022-06-08 10:44:46 +0000

    Change CD url and project

--- src/services/database/index.js
+++ src/services/database/index.js
@@ -1,8 +1,7 @@
 const { Pool } = require('pg');
 
-const connectionString = `postgresql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:5432/postgres`;
 const pool = new Pool({
-  connectionString,
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = pool;
